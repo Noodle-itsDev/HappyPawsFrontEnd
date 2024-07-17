e1a6a6a0bf6d9c80e89ac7bc6122d7beb21af6ad
Borja1998 <160590659+Borja1998@users.noreply.github.com>
2024-07-17 13:50:42 +0200

    cambios de estructura

---
+++ src/index/components/header/header.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import'/Users/Mati/OneDrive/Documentos/GitHub/HappyPawsFrontEnd/src/index/components/header/header.css'
+function Header()
+{
+    return(
+        <>
+        <div id='header'></div>
+        <Link to='/' id='acces'>Acceso</Link>
+     
+        </>
+
+    )
+
+}
+
+export default Header;
