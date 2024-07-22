76a205e0ebbcc00e536b43d117da3850d58e6926
Borja1998 <160590659+Borja1998@users.noreply.github.com>
2024-07-23 00:19:10 +0200

    Index maquetado
    Ya esta maquetado falta pasarlo a tailwindcss y que sea interactivo

--- src/index/components/footer/footer.tsx
+++ src/index/components/footer/footer.tsx
@@ -7,9 +7,16 @@ function FooterHome()
 {
     return(
         <>
-        <img src={img12} id="background-footer"/>
+        <img 
+        src={img12} 
+        id="background-footer"
+        className="flex negative w-[96vw] h-[80vh] -bottom-[292rem] z-2 "
+        />
+        <b id="cuidar-title">Cuidar</b>
         <div id="cuidar-section"></div>
+        <b id="donar-title">Donar</b>
         <div id="donar-section"></div>
+        <b id="adoptar-title">Adoptar</b>
         <img src={img13} id="adoptar-section"/>
         </>
     )
