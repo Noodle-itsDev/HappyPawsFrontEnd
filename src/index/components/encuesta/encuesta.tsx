689fc5be7cb4920dc558e0d958d5b188f6a374bc
Borja1998 <160590659+Borja1998@users.noreply.github.com>
2024-07-24 13:52:30 +0200

    menu
    Tengo todo hecho solo me falta el menu pendiente, me pongo por la tarde

--- src/index/components/encuesta/encuesta.tsx
+++ src/index/components/encuesta/encuesta.tsx
@@ -1,7 +1,50 @@
+import React,{useState, ChangeEvent } from 'react';
 import'./encuesta.css'
 
+interface Protectoras
+{
+  tipo:string;
+  provincia:string;
+  protectora: string;
+}
 const Encuesta:React.FC = () =>
 {
+
+  const[animal, setAnimal] = useState<string>('');
+  const[provincia, setProvincia] = useState<string>('');
+  const[resultado, setResultado] = useState <Protectoras[]>([]);
+
+  const opcionAnimal = (e: ChangeEvent<HTMLSelectElement>)=>{
+
+    setAnimal(e.target.value);
+  };
+
+
+  const opcionProvincia = (e: ChangeEvent <HTMLSelectElement>) =>
+    {
+      setProvincia(e.target.value);
+    };
+
+    const filterBuscar = async() =>
+    {
+      try{
+        const respuesta = await fetch ('http://localhost:3001/buscar',{
+
+          method:'POST',
+          headers: {'Content-Type': 'aplication.json'},
+          body: JSON.stringify({animal,provincia}),
+        }) ;
+        const data: Protectoras[] = await respuesta.json();
+        setResultado(data);
+
+      } catch(error)
+      {
+        console.error('Busqueda no encontrada', error)
+      }
+      
+    };
+
+
     return(
         <>
         <div id="fondo"></div>
@@ -13,21 +56,20 @@ const Encuesta:React.FC = () =>
        
  <form>
       <label htmlFor="animals">
-        <select name="animals" id="form">
+        <select name="animals" id="form" onChange={opcionAnimal}>
             <option value=""></option>
             <option value="perro">Perros</option>
             <option value="gatos">Gatos</option>
         </select>
       </label>
-   </form>
-       
+  </form>
 
-       <p id='txt1'> Provincias</p>
 
-               
- <form>
-      <label htmlFor="animals">
-        <select name="animals" id="form">
+      <p id='txt1'> Provincias</p>
+
+    <form>
+      <label htmlFor="Provincia">
+        <select name="provincia" id="form" onChange={opcionProvincia}>
             <option value=""></option>
             <option value="Barcelona">Barcelona</option>
             <option value="Girona">Girona</option>
@@ -35,19 +77,30 @@ const Encuesta:React.FC = () =>
             <option value="Tarragona">Tarragona</option>
         </select>
       </label>
-   </form>
+    </form>
 
-   <p id='txt1'> Protectoras</p>
+    <p id='txt1'> Protectoras</p>
 
-   <form>
-      <label htmlFor="animals">
-        <select name="animals" id="form">
+    <form>
+      <label htmlFor="shelters">
+        <select name="shelters" id="form">
             <option value=""></option>
         </select>
       </label>
-   </form>
+       <button id='btn' onClick={filterBuscar}><b>Buscar</b></button>
+    </form>
 
-   <button id='btn'><b>Buscar</b></button>
+      <div>
+        {resultado.map ((item, index) => (
+
+          <div key={index}>
+            <p>Tipo:{item.tipo}</p>
+            <p>Provincia:{item.provincia}</p>
+            <p>Protectora:{item.protectora}</p>
+
+          </div>
+        ))}
+      </div>
 
 
         </>
--- src/index/components/menu/menu.tsx
+++ src/index/components/menu/menu.tsx
@@ -1,20 +1,22 @@
 
 import type { SVGProps } from 'react';
 import'../menu/menu.css'
-import Info from '../info/info';
 
 
 export function Menu(props: SVGProps<SVGSVGElement> ) {
 
-    const desplegar  = () =>{
-       
-    
-};
+
 	return (
+
+    <div>
     <svg id='menu' xmlns="http://www.w3.org/2000/svg" 
     width="1em" 
     height="1em" viewBox="0 0 24 24" {...props}>
-    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={2.5} d="M5 7h14M5 12h14M5 17h14" onClick={desplegar}/>
+    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={2.5} d="M5 7h14M5 12h14M5 17h14" />
   
-    </svg>);
+    </svg>
+
+
+    </div>
+    )
 }
